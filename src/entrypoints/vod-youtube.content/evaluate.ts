1d5ffb99288ff18c39dab834ef8a0b6371f662a9
FanaticPond3462 <111404437+FanaticPond3462@users.noreply.github.com>
2025-02-02 18:22:03 +0900

    YouTube用のエピソードおよびシーズン情報の抽出機能を追加し、関連する依存関係を更新

---
+++ src/entrypoints/vod-youtube.content/evaluate.ts
@@ -0,0 +1,142 @@
+import type { ASTResult, ASTElement, ASTBlock } from "./parser";
+import { dump } from "./parser";
+import { episode, season } from "./extractor";
+
+type EvaluatedEpisode = {
+    title: string | null
+    number: number
+}
+
+type EvaluatedMeta = {
+    title: string | null
+    season: string | null
+    episodes: EvaluatedEpisode[]
+}
+
+const isBlock = (x: ASTResult): x is ASTBlock => x.type === "Block";
+
+export const evalAST = (ast: ASTResult[]): EvaluatedMeta => {
+    const evaluatedMeta: EvaluatedMeta = {
+        title: null,
+        season: null,
+        episodes: []
+    };
+
+    let title: ASTElement | null = null;
+    let force_title = false;
+    let subtitleParts: ASTResult[] = [];
+    let episodeRanges: { from: number, to: number } | null = null;
+    let previous: ASTElement | null = null;
+
+    ast.forEach((element, i, array) => {
+        if (element.type === "Season") {
+            if (!evaluatedMeta.season) {
+                evaluatedMeta.season = element.content;
+            }
+        } else if (element.type === "Episode") {
+            const episodeParsed = episode(element.content)[0]
+            const episodeNumber = episodeParsed.number;
+            if (episodeNumber !== null) {
+                if (episodeRanges) {
+                    if (episodeNumber < episodeRanges.to) {
+                        return
+                    }
+                }
+                if (episodeParsed.from_number !== null && episodeParsed.from_number !== undefined) {
+                    episodeRanges = { from: episodeParsed.from_number, to: episodeNumber };
+                } else {
+                    episodeRanges = { from: episodeNumber, to: episodeNumber };
+                }
+            }
+        } else if (isBlock(element)) {
+            element.content.forEach((e) => {
+                if (e.type === "Season") {
+                    if (!evaluatedMeta.season) {
+                        evaluatedMeta.season = e.content;
+                    }
+                } else if (e.type === "Episode") {
+                    const episodeParsed = episode(e.content)[0]
+                    const episodeNumber = episodeParsed.number;
+                    if (episodeNumber !== null) {
+                        if (episodeRanges) {
+                            if (episodeNumber < episodeRanges.to) {
+                                return
+                            }
+                        }
+                        if (episodeParsed.from_number !== null && episodeParsed.from_number !== undefined) {
+                            episodeRanges = { from: episodeParsed.from_number, to: episodeNumber };
+                        } else {
+                            episodeRanges = { from: episodeNumber, to: episodeNumber };
+                        }
+                    }
+                } else if (["(", "〔"].includes(element.prefix ?? "")) {
+                    return
+                } else if (e.type === "Constant" || e.type === "Unknown") {
+                    if (previous?.type === "Episode" || previous?.prefix != "「") {
+                        subtitleParts.push(e);
+                    } else if (!title || force_title) {
+                        title = e;
+                        force_title = false;
+                    } else {
+                        subtitleParts.push(e);
+                    }
+                }
+                if (!isBlock(e))
+                    previous = e
+            })
+        } else if (["(", "〔"].includes(element.prefix ?? "")) {
+            return
+        } else if (element.type === "Constant" || element.type === "Unknown") {
+            if (previous?.type === "Episode") {
+                subtitleParts.push(element);
+            } else if (!title || force_title) {
+                title = element;
+                force_title = false;
+            } else {
+                subtitleParts.push(element);
+            }
+        } else if (element.type == "Promo") {
+            if (["アニメ", "公式"].some(v => element.content.includes(v))) {
+                force_title = true
+            }
+        }
+        if (!isBlock(element) && element.type !== "Separator")
+            previous = element
+    });
+    if (title) evaluatedMeta.title = dump(title) || null;
+    const subtitle = subtitleParts.map((e) => {
+        if (e.prefix == "「", isBlock(e)) {
+            return dump(e.content[0])
+        } else {
+            return dump(e)
+        }
+    }) || null;
+    if (!evaluatedMeta.title) {
+        evaluatedMeta.title = subtitle.pop() || null
+    }
+
+    if (episodeRanges !== null) {
+        if (episodeRanges.from !== undefined && episodeRanges.to !== undefined) {
+            for (let i = episodeRanges.from; i <= episodeRanges.to; i++) {
+                evaluatedMeta.episodes.push({
+                    number: i,
+                    title: subtitle[i - episodeRanges.from] || null
+                });
+            }
+        } else if (episodeRanges.from) {
+            evaluatedMeta.episodes.push({
+                number: episodeRanges.from,
+                title: subtitle[0] || null
+            });
+        }
+    } else if (evaluatedMeta.episodes.length === 0 && (evaluatedMeta.title || subtitle)) {
+        // if no episode declared but title or subtitle exists, consider it episode 0
+        evaluatedMeta.episodes.push({
+            number: 0,
+            title: subtitle[0] || null
+        })
+    }
+
+
+    return evaluatedMeta;
+}
---
+++ src/entrypoints/vod-youtube.content/parser.ts
@@ -0,0 +1,292 @@
+import { episode, season } from "./extractor";
+import { REGEXP_NUMBER } from "@midra/nco-parser/extract/lib/core";
+
+const SEPARATORS = /^['"『』【】\(\)「」｢｣《》/〈〉＜＞\[\]〔〕\s|｜／│・（）~”-]+/;
+const BRACKET_MAP: { [key: string]: string } = {
+    "「": "」", "｢": "｣", "『": "』", "【": "】", "(": ")", "（": "）", "《": "》", "〈": "〉", "'": "'", '"': '"', "〔": "〕", "[": "]", "”": "”",
+};
+
+const REGEXPS = [new RegExp(`(${REGEXP_NUMBER})(?:st|nd|rd|th)`, 'di'), new RegExp(`season`, 'dgi')];
+const SECOND_REGEXPS = [new RegExp(`season`, 'dgi'), new RegExp(`(${REGEXP_NUMBER})(?=\\s|$)`, 'dgi')];
+
+export type ASTElement = {
+    content: string;
+    type: "Constant" | "Episode" | "Season" | "Hashtag" | "Unknown" | "Promo" | "Date" | "Separator" | "Adjunct";
+    prefix: string | null;
+    suffix: string | null;
+}
+
+export type ASTBlock = {
+    content: ASTResult[];
+    type: "Block";
+    prefix: string | null;
+    suffix: string | null;
+}
+
+export type ASTResult = ASTElement | ASTBlock;
+
+const isBlock = (x: any): x is ASTBlock => x.type === "Block";
+
+function splitStringByRanges(str: string, ranges: { start: number, end: number, type: ASTElement["type"] }[]): ASTElement[] {
+    const result: ASTElement[] = [];
+    let lastIndex = 0;
+
+    for (const { start, end, type } of ranges) {
+        if (start > lastIndex) {
+            result.push({
+                content: str.substring(lastIndex, start),
+                type: "Unknown",
+                prefix: null,
+                suffix: null
+            });
+        }
+        result.push({
+            content: str.substring(start, end + 1),
+            type,
+            prefix: null,
+            suffix: null
+        });
+        lastIndex = end + 1;
+    }
+
+    if (lastIndex < str.length) {
+        result.push({
+            content: str.substring(lastIndex),
+            type: "Unknown",
+            prefix: null,
+            suffix: null
+        });
+    }
+
+    return result;
+}
+
+export const genAST = (tokens: string[]): ASTResult[] => {
+    const result: ASTResult[] = [];
+
+    const tokensLength = tokens.length;
+    for (let i = 0; i < tokensLength; i++) {
+        const token = tokens[i];
+        if (BRACKET_MAP[token]) {
+            const prefix = token;
+            const suffix = BRACKET_MAP[token];
+            const contents: string[] = [];
+
+            while (tokens[++i] !== suffix && i < tokens.length) {
+                contents.push(tokens[i]);
+            }
+
+            result.push({
+                content: prefix === "「" || prefix === "『" || prefix === "｢" ? [{
+                    content: contents.join(""),
+                    type: "Constant",
+                    prefix: null,
+                    suffix: null
+                }] : genAST(contents),
+                type: "Block",
+                prefix,
+                suffix
+            });
+            continue;
+        }
+
+        let combinedToken = token;
+
+        REGEXPS.forEach((re, ind) => {
+            if (re.test(token)) {
+                let n = 0;
+                while (tokens[i + ++n] && tokens[i + n] === " ") {
+                    combinedToken += tokens[i + n];
+                }
+                if (SECOND_REGEXPS[ind].test(tokens[i + n])) {
+                    combinedToken += tokens[i + n];
+                    i += n;
+                }
+            }
+        });
+
+        const episodeExtracted = episode(combinedToken.trim(), true);
+        const seasonExtracted = season(combinedToken.trim());
+
+        const ranges = [
+            ...episodeExtracted.map(e => ({ start: e.range[0], end: e.range[1], type: "Episode" as const })),
+            ...seasonExtracted.map(e => ({ start: e.range[0], end: e.range[1], type: "Season" as const }))
+        ];
+
+        const tempResult = ranges.length > 0 ? splitStringByRanges(combinedToken, ranges) : [{
+            content: token,
+            type: "Unknown",
+            prefix: null,
+            suffix: null
+        } as ASTElement];
+
+        tempResult.forEach(element => {
+            if (element.type === "Unknown") {
+                if (element.content.startsWith("#")) {
+                    result.push({
+                        content: element.content.slice(1),
+                        type: "Hashtag",
+                        prefix: element.content[0],
+                        suffix: null
+                    });
+                    return;
+                }
+                if (["期間限定", "見逃し", "公式", "アニメ", "全話", "イッキ見", "更新", "無料", "映画", "特別公開", "周年", "フル", "full"].some(v => element.content.includes(v))) {
+                    result.push({
+                        content: element.content,
+                        type: "Promo",
+                        prefix: element.prefix,
+                        suffix: element.suffix
+                    });
+                    return;
+                }
+                if (/\d+年|\/\d+月|\/\d+日?/.test(element.content)) {
+                    let suffix = "";
+                    for (let o = 1; tokens[i + o]; o++) {
+                        if (tokens[i + o] === " ") {
+                            suffix += tokens[i + o];
+                        } else if ("(日月火水木金土".includes(tokens[i + o])) {
+                            suffix += tokens[i + o];
+                        } else if (tokens[i + o] === ")") {
+                            suffix += tokens[i + o];
+                            i += o;
+                            break;
+                        }
+                    }
+                    result.push({
+                        content: element.content + suffix,
+                        type: "Date",
+                        prefix: element.prefix,
+                        suffix: element.suffix
+                    });
+                    return;
+                }
+                if (/\d+[:(時間?)]\d+分?まで?/.test(element.content)) {
+                    result.push({
+                        content: element.content,
+                        type: "Date",
+                        prefix: element.prefix,
+                        suffix: element.suffix
+                    });
+                    return;
+                }
+            }
+            result.push(element);
+        });
+    }
+
+    const mergedResult: ASTResult[] = [];
+    let tempUnknowns: ASTElement[] = [];
+
+    for (const element of result) {
+        if (element.type === "Unknown") {
+            tempUnknowns.push(element);
+        } else {
+            if (tempUnknowns.length) {
+                const separators = []
+                while (true) {
+                    let isSep: ASTElement | null = tempUnknowns.pop()!;
+                    if (!SEPARATORS.test(isSep.content)) {
+                        tempUnknowns.push(isSep);
+                        isSep = null;
+                    } else {
+                        separators.push(isSep)
+                    }
+                    if (!tempUnknowns.length || isSep === null) {
+                        break;
+                    }
+                }
+                while (true) {
+                    if (tempUnknowns.length && SEPARATORS.test(tempUnknowns[0].content)) {
+                        const sep_element = tempUnknowns.shift()!;
+                        sep_element.type = "Separator";
+                        mergedResult.push(sep_element);
+                    } else {
+                        break;
+                    }
+                }
+                if (tempUnknowns.length) {
+                    mergedResult.push({
+                        content: tempUnknowns.map(e => e.content).join(""),
+                        type: "Unknown",
+                        prefix: null,
+                        suffix: null
+                    });
+                    tempUnknowns = [];
+                }
+                if (separators.length) {
+                    separators.forEach(e => {
+                        e.type = "Separator";
+                        mergedResult.push(e);
+                    })
+                }
+            }
+            mergedResult.push(element);
+        }
+    }
+
+    if (tempUnknowns.length) {
+        let isSep: ASTElement | null = tempUnknowns.pop()!;
+        if (!SEPARATORS.test(isSep.content)) {
+            tempUnknowns.push(isSep);
+            isSep = null;
+        }
+        if (tempUnknowns.length && SEPARATORS.test(tempUnknowns[0].content)) {
+            const sep_element = tempUnknowns.shift()!;
+            sep_element.type = "Separator";
+            mergedResult.push(sep_element);
+        }
+        if (tempUnknowns.length) {
+            mergedResult.push({
+                content: tempUnknowns.map(e => e.content).join(""),
+                type: "Unknown",
+                prefix: null,
+                suffix: null
+            });
+            tempUnknowns = [];
+        }
+        if (isSep) {
+            isSep.type = "Separator";
+            mergedResult.push(isSep);
+        }
+    }
+    mergedResult.forEach((e, i) => {
+        if (mergedResult[i - 1] && mergedResult[i - 1].type == "Episode" && e.type === "Unknown") {
+            e.type = "Adjunct"
+        }
+        if (mergedResult[i + 1] && mergedResult[i + 1].type == "Episode" && e.type === "Unknown") {
+            e.type = "Adjunct"
+        }
+    })
+
+    return mergedResult;
+};
+
+export const tokenize = (target: string): string[] => {
+    const tokens: string[] = [];
+    let currentPart = "";
+    for (const char of target) {
+        if (SEPARATORS.test(char)) {
+            if (currentPart) {
+                tokens.push(currentPart);
+                currentPart = "";
+            }
+            tokens.push(char);
+        } else {
+            currentPart += char;
+        }
+    }
+
+    if (currentPart) {
+        tokens.push(currentPart.trim());
+    }
+
+    return tokens;
+}
+
+export const dump = (x: ASTResult): string => {
+    if (isBlock(x)) {
+        return (x.prefix ?? "") + x.content.map(y => dump(y)).join("") + (x.suffix ?? "");
+    }
+    return (x.prefix ?? "") + x.content + (x.suffix ?? "");
+}
